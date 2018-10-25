af0c44eddf9777d9db978dd8ae18d7b69fb57b1e
Craig Francis <craig@craigfrancis.co.uk>
2018-10-25 14:20:42 +0100

    Layout updates, now that document.body.scrollTop is always 0, and clientWidth does not work in BackCompat

--- chrome/screenshot.js
+++ chrome/screenshot.js
@@ -50,55 +50,80 @@
 
 	chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 
-			if (request.action === 'screenShotResponse') {
+			if (request.action === 'screenShotResponse' && typeof request.screenShotUrl !== 'undefined' && request.screenShotUrl != 'undefined') {
 
-				if (request.screenShotId != screenShotId) {
-					// console.log('Skipped (' + request.screenShotId + ' != ' + screenShotId + ')');
-					return false;
-				}
+				//--------------------------------------------------
+				// Only show the latest
 
-				removeScreenShot();
+					if (request.screenShotId != screenShotId) {
+						// console.log('Skipped (' + request.screenShotId + ' != ' + screenShotId + ')');
+						return false;
+					}
 
-				// console.log('apply');
+				//--------------------------------------------------
+				// Cleanup
 
-				window.addEventListener('scroll', updateScreenShot);
-				window.addEventListener('resize', updateScreenShot);
+					removeScreenShot();
 
-				var windowZoomed = (window.outerWidth != window.innerWidth);
+				//--------------------------------------------------
+				// Events needing a new screenshot
 
-				// Compatibility mode keeps changing.
-				// var windowWidth = (document.compatMode !== 'BackCompat' ? document.documentElement.clientWidth : document.body.clientWidth);
-				// var windowHeight = (document.compatMode !== 'BackCompat' ? document.documentElement.clientHeight : document.body.clientHeight);
+					window.addEventListener('scroll', updateScreenShot);
+					window.addEventListener('resize', updateScreenShot);
 
-				// Returns the full height of the page, causing the scroll bars to change (mostly the vertical scroll bar).
-				// var windowWidth = (document.body.scrollWidth - (windowZoomed ? 1 : 0)); // Hide the scroll bar in screen-shot, and allow for rounding issues when zoomed
-				// var windowHeight = (document.body.scrollHeight - (windowZoomed ? 1 : 0));
+				//--------------------------------------------------
+				// Window size:
 
-				var windowWidth = (document.documentElement.clientWidth - (windowZoomed ? 1 : 0)); // Hide the scroll bar in screen-shot, and allow for rounding issues when zoomed
-				var windowHeight = (document.documentElement.clientHeight - (windowZoomed ? 1 : 0));
+						// Get the size of the window for the wrapper <div>.
+						// This must match the viewable area, and not include the scroll bars.
+						//
+						// We allow the <img> to be the full size, as the image includes
+						// the scroll bars, but are (mostly) hidden by the overflow:hidden <div>.
 
-				screenShotImg = document.createElement('img');
-				screenShotImg.src = request.screenShotUrl;
-				screenShotImg.style.display = 'block';
-				screenShotImg.style.maxWidth = 'none'; // Don't inherit from site css
-				screenShotImg.width = window.innerWidth; // Width with scroll bars, important when zoomed
-				screenShotImg.onclick = removeScreenShot;
+					var windowWidth = (document.compatMode !== 'BackCompat' ? document.documentElement.clientWidth : document.body.clientWidth);
+					var windowHeight = (document.compatMode !== 'BackCompat' ? document.documentElement.clientHeight : document.body.clientHeight);
 
-				screenShotDiv = document.createElement('div');
-				screenShotDiv.appendChild(screenShotImg);
-				screenShotDiv.style.position = 'absolute';
-				screenShotDiv.style.cursor = 'pointer';
-				screenShotDiv.style.top = document.body.scrollTop + 'px';
-				screenShotDiv.style.left = document.body.scrollLeft + 'px';
-				screenShotDiv.style.width = windowWidth + 'px'; // Matt Saul is using `windowWidth + 17`, which might be a Windows Scroll bar issue? (https://github.com/intercision/stop-animations)
-				screenShotDiv.style.height = windowHeight + 'px';
-				screenShotDiv.style.zIndex = 2147483647; // Always on top
-				screenShotDiv.style.overflow = 'hidden';
+					var windowZoomed = (window.outerWidth != window.innerWidth);
+					if (windowZoomed) {
+						windowWidth -= 1;
+						windowHeight -= 1;
+					}
 
-				var bodyRef = document.getElementsByTagName('body');
-				if (bodyRef[0]) {
-					bodyRef[0].appendChild(screenShotDiv);
-				}
+					if (windowHeight > window.innerHeight) { // innerHeight includes the scrollbar, so should always be bigger than the calculated windowHeight... but if it was effectively the document height (hence the compatMode check), we need to limit it.
+						windowHeight = window.innerHeight;
+					}
+
+				//--------------------------------------------------
+				// Image
+
+					screenShotImg = document.createElement('img');
+					screenShotImg.src = request.screenShotUrl;
+					screenShotImg.style.display = 'block';
+					screenShotImg.style.maxWidth = 'none'; // Don't inherit from site css
+					screenShotImg.width = window.innerWidth; // Width with scroll bars, can be wider than the <div> (important when zoomed)
+					screenShotImg.onclick = removeScreenShot;
+
+				//--------------------------------------------------
+				// Wrapper div
+
+					screenShotDiv = document.createElement('div');
+					screenShotDiv.appendChild(screenShotImg);
+					screenShotDiv.style.position = 'absolute';
+					screenShotDiv.style.cursor = 'pointer';
+					screenShotDiv.style.top = window.scrollY + 'px'; // Was document.body.scrollTop (http://lists.w3.org/Archives/Public/www-style/2013Oct/0287.html)
+					screenShotDiv.style.left = window.scrollX + 'px';
+					screenShotDiv.style.width = windowWidth + 'px'; // Matt Saul is using `windowWidth + 17`, which might be a Windows Scroll bar issue? (https://github.com/intercision/stop-animations)
+					screenShotDiv.style.height = windowHeight + 'px';
+					screenShotDiv.style.zIndex = 2147483647; // Always on top
+					screenShotDiv.style.overflow = 'hidden';
+
+				//--------------------------------------------------
+				// Add to page
+
+					var bodyRef = document.getElementsByTagName('body');
+					if (bodyRef[0]) {
+						bodyRef[0].appendChild(screenShotDiv);
+					}
 
 			}
 
